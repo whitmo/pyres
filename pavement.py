c261e408650d58381cdcba35208806a2a440ccef
Whit Morriss <wmorriss@3441702.local>
2010-01-14 15:08:26 -0600

    make install and launch work

--- pavement.py
+++ pavement.py
@@ -1,5 +1,8 @@
+from __future__ import with_statement
 from paver.easy import *
+import tarfile
 import os
+
 
 options(
     current_env = path(os.environ.get('VIRTUAL_ENV', '.')),
@@ -14,7 +17,7 @@ options(
         ),
     redis = Bunch(url="http://redis.googlecode.com/files/redis-1.2.0.tar.gz",
                   script="%(venv)/bin/",
-                  default_port='',
+                  default_port='6379',
                   default_conf=''),
     )
 
@@ -22,6 +25,7 @@ options(
 def install_build_reqs(options):
     for pkg in options.virtualenv.packages:
         sh("pip install %s" %pkg)
+
 
 @task
 @needs('install_build_reqs')
@@ -34,11 +38,12 @@ def get_filename_and_dir(url):
     dirname = filename.split('.tar.gz')[0]
     return filename, dirname
 
+
 def grab_unpack(url, dest):
     from urlgrabber.grabber import urlgrab, URLGrabError
     from urlgrabber.progress import text_progress_meter
-    urlgrab(url, dest, progress_obj=text_progress_meter())
     filename, dirname = get_filename_and_dir(url)
+    urlgrab(url, dest / filename, progress_obj=text_progress_meter())
     with pushd(dest): #necessary?
         mode = "r:gz"
         tarball = tarfile.open(filename, mode)
@@ -49,32 +54,49 @@ def grab_unpack(url, dest):
 @task
 @needs('install_build_reqs')
 def install_redis(options):
+    """
+    Download, unpack and build redis
+    """
     dest = options.current_env / 'src'
-    filename, dirname = get_filename_and_dir(url)
+    filename, dirname = get_filename_and_dir(options.redis.url)
     redis_dir = dest / dirname
     if not redis_dir.exists():
-        redis_dir = grab_unpack(options.redis.url, dest)
+        grab_unpack(options.redis.url, dest)
         with pushd(redis_dir):
             sh('make')
     else:
         info("%s exists" %redis_dir)
 
-    bin = options.current_env / path('bin')
+    options.redis.redis_dir = redis_dir
+    call_task('make_redis_links')
 
+
+@task
+def make_redis_links(options):
+    """
+    Symlink redis executables into virtualenv bin
+    """
+    bin = options.current_env / path('bin')    
     server = bin / 'redis-server'
     cli = bin / 'redis-cli'
+    dest = options.current_env / 'src'
+    filename, dirname = get_filename_and_dir(options.redis.url)
+    redis_dir = dest / dirname
     if not cli.exists():
-        # symlink
-        import pdb;pdb.set_trace()
+        (redis_dir / 'redis-cli').symlink(cli)
+        
     if not server.exists():
-        import pdb;pdb.set_trace()
+        (redis_dir / 'redis-server').symlink(server)
 
 
 # add conf flag
 @task
 @needs('install_redis')
 def launch_redis(options):
-    sh("redis-server %s" %options.redis.default_config)
+    try:
+        sh("redis-server %s" %options.redis.default_conf)
+    except KeyboardInterrupt:
+        info("\nredis exiting")
 
 try:
     from paver.virtual import bootstrap
