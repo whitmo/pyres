e40f8b006a866e1bb92b42e48675d11a55491ab8
Whit Morriss <wmorriss@3441702.local>
2010-01-15 14:51:57 -0600

    commenting out unused setup stuff

--- pavement.py
+++ pavement.py
@@ -1,7 +1,7 @@
 from __future__ import with_statement
 
 from paver.easy import *
-from paver.setuputils import setup
+#from paver.setuputils import setup
 import os
 import tarfile
 
@@ -24,25 +24,25 @@ options(
     )
 
 
-setup(
-    name='pyres',
-    version='0.4.1',
-    description='Python Resque clone',
-    author='Matt George',
-    license='MIT',
-    author_email='mgeorge@gmail.com',
-    url='http://github.com/binarydud/pyres',
-    packages=['pyres', 'resweb', 'pyres/failure'],
-    package_data={'resweb': ['templates/*.mustache','media/*']},
-    scripts=['scripts/pyres_worker', 'scripts/pyres_web'],
-    zip_safe = True,
-    install_requires=[
-        'simplejson>=2.0.9',
-        'itty>=0.6.2',
-        'redis>=0.6.0',
-        'pystache>=0.1.0'
-    ],
-)
+## setup(
+##     name='pyres',
+##     version='0.4.1',
+##     description='Python Resque clone',
+##     author='Matt George',
+##     license='MIT',
+##     author_email='mgeorge@gmail.com',
+##     url='http://github.com/binarydud/pyres',
+##     packages=['pyres', 'resweb', 'pyres/failure'],
+##     package_data={'resweb': ['templates/*.mustache','media/*']},
+##     scripts=['scripts/pyres_worker', 'scripts/pyres_web'],
+##     zip_safe = True,
+##     install_requires=[
+##         'simplejson>=2.0.9',
+##         'itty>=0.6.2',
+##         'redis>=0.6.0',
+##         'pystache>=0.1.0'
+##     ],
+## )
 
 
 @task
